3c933b5b5c3ca901ea908a27d5b3b5fd77fec636
xueqianz <xueqian.zhang@intel.com>
2019-05-06 13:08:25 +0800

    stable version

--- server/main.py
+++ server/main.py
@@ -5,36 +5,44 @@ import cv2, time
 from camera import camera
 
 app = Flask(__name__)
-mainFrame = b''
+#mainFrame = b''
+lock = threading.Lock()
 
 @app.route('/')
 def index():
     print('request received')
     return render_template('index.html')
 
-def getStream():
-    global mainFrame
+def getStream(camera):
+    #global mainFrame
     while True:
         #print(mainFrame)
+        #lock.acquire()
+        mainFrame = camera.getFrame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n'+mainFrame+b'\r\n\r\n')
-        time.sleep(0.05)
+        #time.sleep(0.05)
+        #lock.release()
 
 def getFrame(camera):
     global mainFrame
     while True:
+        lock.acquire()
         mainFrame = camera.getFrame()
+        lock.release()
         #print(mainFrame)
 
 @app.route('/getimg')
 def getimg():
-    print(mainFrame)
-    return Response(getStream(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    #print(mainFrame)
+    #lock.acquire()
+    return Response(getStream(camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+    #lock.release()
 
 if __name__=='__main__':
-    cmr = camera(0)
-    frameGenerator = threading.Thread(target=getFrame, args=(cmr,))
-    frameGenerator.start()
+    #cmr = camera(0)
+    #frameGenerator = threading.Thread(target=getFrame, args=(cmr,))
+    #frameGenerator.start()
     app.run(host='0.0.0.0', debug=True, port=5000)
     #app_run = threading.Thread(target=runapp, args=())
     #app_run.start()
